4d9918234116079b2f073e10b6cadfa5fb254c3f
Stef Kerkhofs <kerkhofsstef@hotmail.com>
2018-04-25 10:52:18 +0200

    Added definitions to toddler

--- cameraatje/cameraatje/Models/Toddler.cs
+++ cameraatje/cameraatje/Models/Toddler.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace cameraatje.Models
 {
-    class Toddler
+   public class Toddler
     {
+        public int kleuter_id { get; set; }
+        public int foto_id { get; set; }
+        public string kleuter_naam { get; set; }
+        public string foto_string { get; set; }
+
+        public int kleuter_id_referentie { get; set; }
+        public User User { get; set; }
+
+        
     }
 }
