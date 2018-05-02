c3908a5aa8ae93d528c9eaa4e5f4c9dfabe3f8b8
Stef Kerkhofs <kerkhofsstef@hotmail.com>
2018-05-02 09:42:28 +0200

    Bindingcommand start to login

--- cameraatje/cameraatje/ViewModels/MainPageViewModel.cs
+++ cameraatje/cameraatje/ViewModels/MainPageViewModel.cs
@@ -1,11 +1,13 @@
-﻿using Prism.Commands;
+﻿using cameraatje.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Windows.Input;
+//Stef
 namespace cameraatje.ViewModels
 {
     public class MainPageViewModel : ViewModelBase
@@ -13,7 +15,19 @@ namespace cameraatje.ViewModels
         public MainPageViewModel(INavigationService navigationService) 
             : base (navigationService)
         {
-            Title = "Main Page";
+            Title = "Start";
+            
+            NavigateToLoginCommand = new DelegateCommand(NavigateToLogin);
         }
+
+       
+        private void NavigateToLogin()
+        {
+            var p = new NavigationParameters();
+            p.Add("user", new User());
+            NavigationService.NavigateAsync("Login", p);
+        }
+      
+        public ICommand NavigateToLoginCommand { get; private set; }
     }
 }
