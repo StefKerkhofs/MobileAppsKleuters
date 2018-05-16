3656c675096b846910f687866df2999493cd606e
Stef Kerkhofs <kerkhofsstef@hotmail.com>
2018-05-16 11:04:27 +0200

    overviewpicture

--- cameraatje/cameraatje/ViewModels/OverviewPictureViewModel.cs
+++ cameraatje/cameraatje/ViewModels/OverviewPictureViewModel.cs
@@ -4,17 +4,21 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using cameraatje.Models;
 
 namespace cameraatje.ViewModels
 {
     //Author: Stef Kerkhofs
-	public class OverviewPictureViewModel : ViewModelBase
-	{
+    public class OverviewPictureViewModel : ViewModelBase
+    {
         public OverviewPictureViewModel(INavigationService navigationService) : base(navigationService)
         {
+            Title = "Fotolijst";
 
+            
+       
         }
 
-      
+        private IList<Picture> pictures;
     }
 }
