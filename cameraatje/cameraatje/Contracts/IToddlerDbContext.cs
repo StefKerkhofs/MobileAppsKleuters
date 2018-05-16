6d0847a8bb2870b26c81c1c5fff5f9459119daec
sashavdv <sashavandevoorde@gmail.com>
2018-05-16 09:49:52 +0200

    errors fixed

---
+++ cameraatje/cameraatje/Contracts/IToddlerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using cameraatje.Models;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace cameraatje.Contracts
+{
+    public interface IToddlerDbContext
+    {
+        DbSet<Toddler> Toddlers { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken));
+    }
+}
---
+++ cameraatje/cameraatje/Data/ToddlerDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using cameraatje.Contracts;
+using cameraatje.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace cameraatje.Data
+{
+    public class ToddlerDbContext : DbContext, IToddlerDbContext
+    {
+        public DbSet<Toddler> Toddlers { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        private string connectionString;
+
+        public ToddlerDbContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
